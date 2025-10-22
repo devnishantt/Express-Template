f460eb49383293ec07f935ac6c9626594d65dcea
devnishantt <nishanttgithub@gmail.com>
2025-10-23 01:14:56 +0530

    feat: integrate winston logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { PORT } from "./config/serverConfig.js";
 import apiRouter from "./routes/apiRoutes.js";
+import logger from "./config/loggerConfig.js";
 
 const app = express();
 
 app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+  logger.info(`Server running on http://localhost:${PORT}`);
+  logger.info(`Press Ctrl+C to stop the server`);
 });
